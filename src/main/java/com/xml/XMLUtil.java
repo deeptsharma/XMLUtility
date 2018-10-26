5c9f8481e028ec126f7b4b4ca2d90c9ddb85e554
Deept Sharma <deeptsharma@live.in>
2018-10-26 17:48:06 +0530

    Added Junit

--- src/main/java/com/xml/XMLUtil.java
+++ src/main/java/com/xml/XMLUtil.java
@@ -1,10 +1,9 @@
 package com.xml;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.StringReader;
 import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.StringTokenizer;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -23,17 +22,32 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
+import org.w3c.dom.Text;
 import org.xml.sax.SAXException;
 
 public class XMLUtil {
-	public static Document convertStringToDocument(String inputString) {
-		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+
+	/**
+	 * Function to convert String to Document
+	 * @param xmlString
+	 * @return Document
+	 */
+	public static Document getXMLDocument(String xmlString) {
 		try {
-			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-			return documentBuilder.parse(new InputSource(new StringReader(inputString)));
+			if (xmlString != null) {
+				DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+				/**				documentBuilderFactory.setValidating(true);
+				documentBuilderFactory.setFeature("http://javax.xml.XMLConstants/feature/secure-processing", true);
+				documentBuilderFactory.setFeature("http://xml.org/sax/features/external-parameter-entities ",false);
+				documentBuilderFactory.setFeature("http://xml.org/sax/features/external-general-entities", false);*/
+				DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+				ByteArrayInputStream inputStream = new ByteArrayInputStream(xmlString.getBytes());
+				return documentBuilder.parse(inputStream);
+			}
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		} catch (SAXException e) {
@@ -43,8 +57,13 @@ public class XMLUtil {
 		}
 		return null;
 	}
-
-	public static String convertDocumentToString(Document xmlString) {
+	
+	/**
+	 * Function to Convert Document to String
+	 * @param xmlString
+	 * @return String
+	 */
+	public static String getXMLText(Document xmlString) {
 		TransformerFactory transformerFactory = TransformerFactory.newInstance();
 		try {
 			Transformer transformer = transformerFactory.newTransformer();
@@ -59,32 +78,264 @@ public class XMLUtil {
 		return null;
 	}
 
-	public static List<String> getAttributeValue(Document document, String tagName, String attribute) {
-		List<String> attributeVaueList=new ArrayList<>();
+	/**
+	 * Function to get tag/attribute value from Document
+	 * parent/tag to get tag value	or  parent@attribute to get attribute value 
+	 * @param document
+	 * @param pathString
+	 * @return value
+	 */
+	public static String getValueFromDom(Document document, String inputString) {
+		try {
+			StringTokenizer stringTokenizer=null;
+			stringTokenizer=new StringTokenizer(inputString,"/");
+			if(stringTokenizer.countTokens()>1) {
+				return getTagElementValue(document,stringTokenizer.nextToken(),stringTokenizer.nextToken());
+			}
+			
+			stringTokenizer=new StringTokenizer(inputString,"@");
+			if(stringTokenizer.countTokens()>1) {
+				return getAttributeValue(document,stringTokenizer.nextToken(),stringTokenizer.nextToken());
+			}
+		}
+		catch(Exception ex) {
+			
+		}
+		return null;
+	}
+	
+	/**
+	 * Function to modify tag or attribute value
+	 * parent/tag to modify tag element value  or parent@attribute to modify attribute value
+	 * @param document
+	 * @param pathString
+	 * @param pathValue
+	 */
+	public static void setValueInDom(Document document, String pathString, String pathValue) {
+		try {
+			StringTokenizer stringTokenizer=null;
+			stringTokenizer=new StringTokenizer(pathString,"/");
+			if(stringTokenizer.countTokens()>1) {
+				setTagElementValue(document,stringTokenizer.nextToken(),stringTokenizer.nextToken(),pathValue);
+				return;
+			}
+			
+			stringTokenizer=new StringTokenizer(pathString,"@");
+			if(stringTokenizer.countTokens()>1) {
+				setAttributeValue(document,stringTokenizer.nextToken(),stringTokenizer.nextToken(),pathValue);
+				return;
+			}
+		}
+		catch(Exception ex) {
+			
+		}
+	}
+
+	public static void addAttribute(Document document, String parentTag, String attributeName,String attributeValue) {
+		try {
+			NodeList nodeList=document.getElementsByTagName(parentTag);
+			Node node;
+			if(nodeList.getLength()>0) {
+				node=nodeList.item(nodeList.getLength()-1);
+			}
+			else {
+				node=nodeList.item(0);
+			}	
+
+			String attrVal="";
+			if(attributeValue!=null) {
+				attrVal=attributeValue;
+			}
+			Element nodeElement = (Element)node;
+			nodeElement.setAttribute(attributeName, attrVal);
+		}
+		catch(Exception ex) {
+			
+		}
+	}
+	
+	/**
+	 * Function to get Attribute value
+	 * @param document
+	 * @param Parent tagName
+	 * @param attributeName
+	 * @return value
+	 */
+	public static String getAttributeValue(Document document, String tagName, String attributeName) {
+		String retVal=null;
+		NodeList list= document.getElementsByTagName(tagName);
+		if(list!=null) {
+			Node node= list.item(0);
+			if(node!=null) {
+				NamedNodeMap namedNodeMap=node.getAttributes();
+				Node tempNode=namedNodeMap.getNamedItem(attributeName);
+				if(tempNode!=null) {
+					return tempNode.getNodeValue();
+				}
+			}
+		}
+		return retVal;
+	}
+	
+	/**
+	 * Function to modify attribute value in Document
+	 * @param document
+	 * @param tagName
+	 * @param attributeName
+	 * @param attributeVal
+	 */
+	public static void setAttributeValue(Document document, String tagName, String attributeName, String attributeVal) {
+		try {
+			NodeList nodeList=document.getElementsByTagName(tagName);
+			Node node=nodeList.item(0);
+			NamedNodeMap nodeMap = node.getAttributes();
+			Node attr=nodeMap.getNamedItem(attributeName);
+			if(attr!=null) {
+				attr.setNodeValue(attributeVal);
+			}
+		}
+		catch(Exception ex) {
+			
+		}
+	}
+	
+	/**
+	 * Function to remove Attribute from Document
+	 * @param document
+	 * @param tagName
+	 * @param attributeName
+	 */
+	public static void removeAttribute(Document document, String tagName, String attributeName) {
+		try {
+			Element element = (Element) document.getElementsByTagName(tagName).item(0);
+			boolean hasAttribute=element.hasAttribute(attributeName);
+			if(hasAttribute) {
+				element.removeAttribute(attributeName);
+			}
+		}
+		catch(Exception ex) {
+			
+		}
+	}
+	
+	/**
+	 * Function to add tag to Document
+	 * @param document
+	 * @param parentTag
+	 * @param nodeName
+	 * @param nodeValue
+	 */
+	public static void addTagElement(Document document, String parentTag, String nodeName, String nodeValue) {
+		try {
+			NodeList nodeList=document.getElementsByTagName(parentTag);
+			
+			if(nodeList==null) {
+				return ;
+			}
+			
+			Node node=nodeList.item(0);
+			Element element = document.createElement(nodeName);
+			
+			String newNodeValue;
+			
+			if(nodeValue==null) {
+				newNodeValue="";
+			}
+			else {
+				newNodeValue=nodeValue;
+			}
+			
+			Text text=document.createTextNode(newNodeValue);
+			element.appendChild(text);
+			node.appendChild(element);
+		}
+		catch(Exception ex ) {
+			
+		}
+	}
+	
+	/**
+	 * Function to get tag element value from document
+	 * @param document
+	 * @param parentTag
+	 * @param childTag
+	 * @return value
+	 */
+	public static String getTagElementValue(Document document, String parentTag, String childTag) {
+		if (parentTag == null || childTag == null) {
+			return null;
+		}
+
 		XPath xPath = XPathFactory.newInstance().newXPath();
 		try {
-			XPathExpression expr = xPath.compile("//" + tagName + "[@" + attribute + "]");
+			XPathExpression expr = xPath.compile("//" + parentTag + "/" + childTag);
 			NodeList nl = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
-
-			for (int i = 0; i < nl.getLength(); i++) {
-				Node currentItem = nl.item(i);
-				String key = currentItem.getAttributes().getNamedItem(attribute).getNodeValue();
-				attributeVaueList.add(key);
+			if (nl.getLength() > 0) {
+				Node currentItem = nl.item(0);
+				return currentItem.getTextContent();
 			}
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
-		return attributeVaueList;
-	}
-
-	public static  List<String> getTagElementValue(Document document, String parentTag, String childTag){
-		List<String> tagValueList=new ArrayList<>();
-		
-		
-		
-		return tagValueList;
+		return null;
 	}
 	
+	/**
+	 * Function to modify tag element value
+	 * @param document
+	 * @param parentTag
+	 * @param tagName
+	 * @param tagValue
+	 */
+	public static void setTagElementValue(Document document, String parentTag, String tagName, String tagValue) {
+		try {
+			NodeList nodeList=document.getElementsByTagName(parentTag);
+			Node node=nodeList.item(0);
+			NodeList childNodeList=node.getChildNodes();
+			
+			for(int i=0;i<childNodeList.getLength();i++) {
+				Node childNode=childNodeList.item(i);
+				if(childNode.getNodeName().equalsIgnoreCase(tagName)) {
+					childNode.setTextContent(tagValue);
+					//childNode.setNodeValue(tagValue);
+					break;
+				}
+			}
+		}
+		catch(Exception ex) {
+			
+		}
+	}
+
+	/**
+	 * Function to remove tag from Document
+	 * @param document
+	 * @param parentTag
+	 * @param childTag
+	 */
+	public static void removeTagElement(Document document, String parentTag, String childTag) {
+		if (parentTag == null || childTag == null) {
+			return;
+		}
+
+		XPath xPath = XPathFactory.newInstance().newXPath();
+		try {
+			XPathExpression expr = xPath.compile("//" + parentTag + "/" + childTag);
+			NodeList nl = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+			if (nl.getLength() > 0) {
+				Node currentItem = nl.item(0);
+				currentItem.getParentNode().removeChild(currentItem);
+			}
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Function to get Formatted XML with indentation 2
+	 * @param document
+	 * @return formatted XML
+	 */
 	public static String getFormattedXml(Document document) {
 		Transformer transformer;
 		try {
@@ -103,3 +354,48 @@ public class XMLUtil {
 		return null;
 	}
 }
+
+/**public static String getAttributesValue(Document document, String tagName, String attribute) {
+	XPath xPath = XPathFactory.newInstance().newXPath();
+	try {
+		XPathExpression expr = xPath.compile("//" + tagName + "[@" + attribute + "]");
+		NodeList nl = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+
+		if (nl.getLength() > 0) {
+			Node currentItem = nl.item(0);
+			return currentItem.getAttributes().getNamedItem(attribute).getNodeValue();
+		}
+	} catch (Exception exception) {
+		exception.printStackTrace();
+	}
+	return null;
+}
+public static void setAttributeValue(Document document, String tagName, String attribute, String nodeValue) {
+	XPath xPath = XPathFactory.newInstance().newXPath();
+	try {
+		XPathExpression expr = xPath.compile("//" + tagName + "[@" + attribute + "]");
+		NodeList nl = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+
+		if (nl.getLength() > 0) {
+			Node currentItem = nl.item(0);
+			currentItem.getAttributes().getNamedItem(attribute).setNodeValue(nodeValue);
+		}
+	} catch (Exception exception) {
+		exception.printStackTrace();
+	}
+}
+
+public static void removeAttribute(Document document, String tagName, String attribute) {
+	XPath xPath = XPathFactory.newInstance().newXPath();
+	try {
+		XPathExpression expr = xPath.compile("//" + tagName + "[@" + attribute + "]");
+		NodeList nl = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+
+		if (nl.getLength() > 0) {
+			Node currentItem = nl.item(0);
+			currentItem.getAttributes().removeNamedItem(attribute);
+		}
+	} catch (Exception exception) {
+		exception.printStackTrace();
+	}
+}*/
--- src/test/java/com/xml/XMLUtilTest.java
+++ src/test/java/com/xml/XMLUtilTest.java
@@ -1,18 +1,124 @@
 package com.xml;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.Document;
 
 public class XMLUtilTest {
+	private static final String TAG_PATH="catalog_item/item_number";
+	private static final String  TAG_NAME="item_number";
+	private static final String  ATTRIBUTE_NAME="gender";
+	private static final String  PARENT_TAG="catalog_item";
+	private static final String  ATTRIBUTE_PATH="catalog_item@gender";
+	private static final String  VALUE="Modified Value";
+	
+	private Document document;
+	@Before
+	public void setup() {
+		document=XMLUtil.getXMLDocument(getMessage());
+	}
 	
 	@Test
-	public void test() {
-		Document document=XMLUtil.convertStringToDocument(getMessage());
-		XMLUtil.getTagElementValue(document,"name","id");
+	public  void testGetXMLDocument() {
+		assertNotNull(XMLUtil.getXMLDocument(getMessage()));
 	}
+
+	@Test
+	public  void testGetXMLText() {
+		assertNotNull(XMLUtil.getXMLText(document));
+	}
+
+	@Test
+	public  void testGetValueFromDomTag() {
+		assertNotNull(XMLUtil.getValueFromDom(document, TAG_PATH));
+	}
+	
+	@Test
+	public  void testGetValueFromDomAttribute() {
+		assertNotNull(XMLUtil.getValueFromDom(document, ATTRIBUTE_PATH));
+	}
+	
+	@Test
+	public  void testSetValueInDomTag() {
+		XMLUtil.setValueInDom(document, TAG_PATH, VALUE);
+		assertEquals(XMLUtil.getValueFromDom(document, TAG_PATH), VALUE);	
+	}
+	
+	@Test
+	public  void testSetValueInDomAttribute() {
+		XMLUtil.setValueInDom(document, ATTRIBUTE_PATH, VALUE);
+		assertEquals(XMLUtil.getValueFromDom(document, ATTRIBUTE_PATH), VALUE);	
+	}
+
+	@Test
+	public  void testAddAttribute() {
+		String attrName="NewAttribute";
+		XMLUtil.addAttribute(document, PARENT_TAG, attrName, VALUE);
+		assertNotNull(XMLUtil.getAttributeValue(document, PARENT_TAG, attrName));
+	}
+	
+	@Test
+	public  void testGetAttributeValue() {
+		assertNotNull(XMLUtil.getAttributeValue(document, PARENT_TAG, ATTRIBUTE_NAME));
+	}
+	
+	@Test
+	public  void testSetAttributeValue() {
+		XMLUtil.setAttributeValue(document, PARENT_TAG, ATTRIBUTE_NAME, VALUE);
+		assertEquals(XMLUtil.getValueFromDom(document, ATTRIBUTE_PATH), VALUE);	
+	}
+
+	@Test
+	public  void testRemoveAttribute() {
+		XMLUtil.removeAttribute(document, PARENT_TAG, ATTRIBUTE_NAME);
+		assertNull(XMLUtil.getValueFromDom(document, ATTRIBUTE_PATH));	
+	}
+	
+	@Test
+	public  void testAddTagElement() {
+		String newTag="NewTag";
+		XMLUtil.addTagElement(document, PARENT_TAG, newTag, VALUE);
+		assertNotNull(XMLUtil.getValueFromDom(document, PARENT_TAG+"/"+newTag));
+	}
+	
+	@Test
+	public  void testGetTagElementValue() {
+		assertNotNull(XMLUtil.getTagElementValue(document, PARENT_TAG, TAG_NAME));
+	}
+	
+	@Test
+	public  void testSetTagEelementValue() {
+		XMLUtil.setTagElementValue(document, PARENT_TAG, TAG_NAME, VALUE);
+		assertEquals(XMLUtil.getTagElementValue(document, PARENT_TAG, TAG_NAME),VALUE);
+	}
+
+	@Test
+	public  void testRemoveTagElement() {
+		XMLUtil.removeTagElement(document, PARENT_TAG, TAG_NAME);
+		assertNull(XMLUtil.getTagElementValue(document, PARENT_TAG, TAG_NAME));
+	}
+	
+	@Test
+	public  void testGetFormattedXml() {
+		assertNotNull(XMLUtil.getFormattedXml(document));
+	}
+	
 	public String getMessage() {
-	return "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"+
-            "<Emp id=\"1\"><name  id=\"10\">Pankaj</name><age>25</age>\n"+
-            "<role>Developer</role><gen>Male</gen></Emp>";
+	return "<catalog>"+
+			   "<product description=\"Cardigan Sweater\" product_image=\"cardigan.jpg\">"+
+			      "<catalog_item gender=\"Mens\">"+
+			         "<item_number no=\"10\">QWZ5671</item_number>"+
+			         "<price>39.95</price>"+
+			         "<size description=\"Medium\">"+
+			            "<color_swatch image=\"red_cardigan.jpg\">Red</color_swatch>"+
+			            "<color_swatch image=\"burgundy_cardigan.jpg\">Burgundy</color_swatch>"+
+			         "</size>"+
+			      "</catalog_item>"+
+			   "</product>"+
+			"</catalog>";
 	}
 }
